7ffb1ee47fbfa7f3f0139268a99262eceae81b92
KazikKluz <kazik.kluz@gmail.com>
2020-09-12 15:36:25 +0100

    adding items to cart partially implemented 243

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,14 +1,27 @@
 import { gql } from "apollo-boost";
 
+import { addItemToCart } from "./cart.utils";
+
 export const typeDefs = gql`
   extend type Mutation {
     ToggleCartHidden: Boolean!
+    AddItemToCart(item: Item!): [Item]!
+  }
+
+  extend type Item {
+    quantity: Int
   }
 `;
 
 export const GET_CART_HIDDEN = gql`
   {
     cartHidden @client
+  }
+`;
+
+export const GET_CART_ITEMS = gql`
+  {
+    cartItems @client
   }
 `;
 
@@ -25,6 +38,20 @@ export const resolvers = {
       });
 
       return !cartHidden;
+    },
+
+    addItemToCart: (_root, { item }, { cache }) => {
+      const { cartItems } = cache.readQuery({
+        query: GET_CART_ITEMS,
+      });
+
+      const newCartItems = addItemToCart(cartItems, item);
+      cache.writeQuery({
+        query: GET_CART_ITEMS,
+        data: { cartItems: newCartItems },
+      });
+
+      return newCartItems;
     },
   },
 };
