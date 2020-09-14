62db66ee2459cb405236ae4f5ab7277805c4c8fb
KazikKluz <kazik.kluz@gmail.com>
2020-09-14 22:25:34 +0100

    mutation on the client partially implemented 240

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
 import { store, persistor } from "./redux/store";
+import { resolvers, typeDefs } from "./graphql/resolvers";
 
 import "./index.css";
 import App from "./App";
@@ -24,25 +25,11 @@ const client = new ApolloClient({
   cache,
 });
 
-client
-  .query({
-    query: gql`
-      {
-        getCollectionsByTitle(title: "hats") {
-          id
-          title
-          items {
-            id
-            name
-            price
-            imageUrl
-          }
-        }
-      }
-    `,
-  })
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err.message));
+client.writeData({
+  data: {
+    cartHidden: true,
+  },
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
